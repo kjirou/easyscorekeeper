e76db93306b3c2a83ceb3334a111cb014e2b3a9f
kjirou <kjirou.web@gmail.com>
2013-02-12 01:11:07 +0900

    Added score registeration function

--- includes/easyranker.php
+++ includes/easyranker.php
@@ -8,6 +8,7 @@
  * @author kjirou <sorenariblog[at]google[dot]com> <http://blog.kjirou.net/>
  */
 class EasyRankerError extends Exception {}
+class EasyRankerNgError extends EasyRankerError {}
 
 class EasyRanker {
 
@@ -16,56 +17,75 @@ class EasyRanker {
     private $data_root = './data';
     private $db_filename = 'easyranker.db';
     private $db = null;
+
     private $params = array();
+
+    private $mode_key = 'm';
+    private $mode = null;
+    private $all_modes = array('list', 'new');
+
+    private $jsonp_callback_key = 'c';
+    /** jQuery.ajax send strings like 'jQuery1705597869567432656_1326204897902' */
+    private $jsonp_callback = null;
+
+    private $username_key = 'u';
+    private $username = null;
+    private $unknown_username = 'unknown';
 
     public function execute() {
 
-        $this->db = $this->initialize_db();
+        $this->initialize_db();
 
-        #$this->apply_mode();
-        #$this->apply_person_id();
-        #$this->apply_jsonp_callback();
+        $this->set_mode_from_params();
+        $this->set_jsonp_callback_from_params();
+        $this->set_username_from_params();
 
-        #// データディレクトリが無ければ生成する
-        #if (file_exists($this->data_dir) === false) {
-        #    mkdir($this->data_dir); //! 第2引数に 0777 入れたけどダメだった、詳細不明
-        #    chmod($this->data_dir, 0777);
-        #}
+        try {
+          if ($this->mode === 'list') {
+            throw new EasyRankerNgError('Not implemented');
+          } else if ($this->mode === 'new') {
+            $this->execute_new();
+          }
+          $this->db->close();
+          $this->output_response('ok');
+        } catch (EasyRankerNgError $err) {
+          $this->db->close();
+          $this->output_response('ng', null, $err->getMessage());
+          return;
+        }
+    }
 
-        #// .htaccess自動生成オプション
-        #if ($this->enable_auto_htaccess) {
-        #    $htaccess_path = $this->data_dir . '/.htaccess';
-        #    if (file_exists($htaccess_path) === false) {
-        #        $fh = fopen($htaccess_path, 'w');
-        #        fwrite($fh, "Order Deny,Allow\nDeny from All");
-        #        fclose($fh);
-        #        chmod($htaccess_path, 0777);
-        #    }
-        #}
+    private function execute_new() {
 
-        ##// データファイル削除処理, 数アクセスに1回実行する
-        ##if ($this->gc_frequency !== null && rand(1, $this->gc_frequency) === 1) {
-        ##    $this->clean_data_file();
-        ##}
+        // Score
+        $score_str = @$this->params['score'];
+        if (is_numeric($score_str) === false) {
+            throw new EasyRankerNgError('Invalid score');
+        }
+        $score = intval($score_str);
 
-        #// エンドユーザによる誤った操作は EasyKVSNgResponseError を投げる
-        #try {
-        #    if ($this->person_id === null) {
-        #        throw new EasyKVSNgResponseError("None `{$this->person_id_key}` in params");
-        #    }
-        #    if ($this->mode === 'fetch') {
-        #        return $this->fetch();
-        #    } else if ($this->mode === 'update') {
-        #        return $this->update();
-        #    } else if ($this->mode === 'remove') {
-        #        return $this->remove();
-        #    }
-        #} catch (EasyKVSNgResponseError $err) {
-        #    $this->output_response('ng', $err->getMessage());
-        #    return;
-        #}
+        // Comment
+        $comment = strval(@$this->params['comment']);
+        $comment = substr($comment, 0, 250);
 
-        $this->db->close();
+        $sql = <<<EOF
+          INSERT INTO scores (
+            created_at,
+            username,
+            score,
+            comment
+          ) VALUES (
+            datetime('now'),
+            :username,
+            :score,
+            :comment
+          );
+EOF;
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':username', $this->username, SQLITE3_TEXT);
+        $stmt->bindValue(':score', $score, SQLITE3_INTEGER);
+        $stmt->bindValue(':comment', $comment, SQLITE3_TEXT);
+        $stmt->execute();
     }
 
     private function get_db_filepath() {
@@ -73,11 +93,88 @@ class EasyRanker {
     }
 
     private function initialize_db() {
-      return new SQLite3($this->get_db_filepath());
+      $this->db = new SQLite3($this->get_db_filepath());
+      if ($this->is_created_tables() === false) {
+        $this->create_tables();
+      }
+    }
+
+    private function is_created_tables() {
+      $sql = 'SELECT * FROM sqlite_master WHERE type="table"';
+      $result = $this->db->query($sql);
+      return $result->fetchArray(SQLITE3_ASSOC) !== false;
+    }
+
+    private function create_tables() {
+        $sql = <<<EOF
+          CREATE TABLE "scores" (
+            "id" integer NOT NULL PRIMARY KEY,
+            "created_at" datetime NOT NULL,
+            "username" varchar(255) NOT NULL,
+            "score" integer NOT NULL,
+            "comment" varchar(255) NOT NULL
+          );
+EOF;
+        $this->db->query($sql);
     }
 
     public function add_params($params) {
         $this->params = array_merge($this->params, $params);
+    }
+
+    private function set_mode_from_params() {
+        if (
+            array_key_exists($this->mode_key, $this->params) &&
+            in_array($this->params[$this->mode_key], $this->all_modes, true)
+        ) {
+            $this->mode = $this->params[$this->mode_key];
+        } else {
+            $this->mode = $this->all_modes[0];
+        }
+    }
+
+    private function set_jsonp_callback_from_params() {
+        if (
+            array_key_exists($this->jsonp_callback_key, $this->params) &&
+            preg_match('/^[_$a-zA-Z0-9]{1,128}$/', $this->params[$this->jsonp_callback_key]) > 0
+        ) {
+            $this->jsonp_callback = $this->params[$this->jsonp_callback_key];
+        }
+    }
+
+    private function set_username_from_params() {
+        if (
+            array_key_exists($this->username_key, $this->params) &&
+            preg_match('/^[-_$a-zA-Z0-9]{1,32}$/', $this->params[$this->username_key]) > 0
+        ) {
+            $this->username = $this->params[$this->username_key];
+        } else {
+            $this->username = $this->unknown_username;
+        }
+    }
+
+    private function create_response_body($status, $data = null, $message = nul) {
+        $status = (in_array($status, array('ok', 'ng'), true))? $status: 'ok';
+
+        $json = json_encode(array(
+            'status' => $status,
+            'data' => $data,
+            'message' => $message,
+        ));
+
+        if ($this->jsonp_callback !== null) {
+            return "{$this->jsonp_callback}({$json})";
+        } else {
+            return $json;
+        }
+    }
+
+    private function output_response(/* args passing */) {
+        $args = func_get_args();
+        $response_body = call_user_func_array(array($this, 'create_response_body'), $args);
+        header('Content-type: text/plain');
+        header('HTTP/1.0 200 OK'); // Always 200
+        echo $response_body;
     }
 
     static public function auto_start() {
